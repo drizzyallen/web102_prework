3ef9526cda8adef812856cdadfcc999defbc4b72
Allen Ramirez <allenram.15@yahoo.com>
2025-12-06 14:45:52 -0500

    challenge #3 progress

--- index.js
+++ index.js
@@ -28,23 +28,17 @@ const gamesContainer = document.getElementById("games-container");
 // create a function that adds all data from the games array to the page
 function addGamesToPage(games) {
 
+    gamesContainer.innerHTML = "";
     // loop over each item in the data
+    for (let i = 0; i < games.length; i++){
+        const gameCard = document.createElement('div'); // create a new div element, which will become the game card
+        gameCard.classList.add('game-card'); // add the class game-card to the list
 
+        // set the inner HTML using a template literal to display some info
+        gameCard.innerHTML = `<img class="game-img" src="${games[i].img}"/>`;
 
-        // create a new div element, which will become the game card
-
-
-        // add the class game-card to the list
-
-
-        // set the inner HTML using a template literal to display some info 
-        // about each game
-        // TIP: if your images are not displaying, make sure there is space
-        // between the end of the src attribute and the end of the tag ("/>")
-
-
-        // append the game to the games-container
-
+        gamesContainer.appendChild(gameCard); // append the game to the games-container
+    }
 }
 
 // call the function we just defined using the correct variable
